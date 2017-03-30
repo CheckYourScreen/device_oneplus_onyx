70a6e4d1b8b76c29c109c7cea2072588e038e5bc
CheckYourScreen <nimitmehta95@gmail.com>
2017-03-30 20:13:37 +0200

    onyx: inherit device specific custom dalvik heap props
    * Some roms are having different values for these props, since tree is inheriting them from rom source it might screw up on those which have inappropriate values.
      Include the best values suitable for the device within the tree itself to overccome such issue.
    
    * Increase the heapminfree size from default 512k to 2m to decrease app launch latencies
    
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

---
+++ dalvik-heap.mk
@@ -0,0 +1,11 @@
+# override's default values for xxhdpi-3072 device.
+# increased heapminfree size to 2m from default 512k to improve app launch latencies
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
