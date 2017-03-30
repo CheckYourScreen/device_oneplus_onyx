6b2daa8411e588b4b639f01578c52899c9a23a70
CheckYourScreen <nimitmehta95@gmail.com>
2017-03-30 20:13:37 +0200

    onyx: inherit device specific custom dalvik heap props
    * Some roms are having different values for these props, since tree is inheriting them from rom source it might screw up on those which have inappropriate values.
      Include the best values suitable for the device within the tree itself to overccome such issue.
    
    * Increase the heapminfree size from default 512k to 2m to decrease app launch latencies
    
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, device/oneplus/onyx/dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Overlays
