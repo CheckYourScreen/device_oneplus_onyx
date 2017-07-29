ed56703cf308e77ca406ef5bccbd8f06046eb25b
Stefano Meroni <smeroni68@gmail.com>
2017-07-29 16:26:07 +0200

    Add OTA prop info for OnePlus X

--- device.mk
+++ device.mk
@@ -120,6 +120,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OnyxDoze
 
+# OTA Updates
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ota.romname=AOSP-OnePlus-X-7.1 \
+    ro.ota.version=$(shell date -u +%Y%m%d) \
+    ro.ota.manifest=https://romhut.com/roms/aosp-oneplus-x/ota.xml
+    
 # Graphics
 PRODUCT_PACKAGES += \
     copybit.msm8974 \
